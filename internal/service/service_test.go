a3993de94d20fdb9c4db4b0113865e22fab1f126
tsw303005 <tsw303005@gmail.com>
2022-04-01 23:34:46 +0800

    fix lint

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -34,6 +34,8 @@ var _ = Describe("Service", func() {
 	)
 
 	const shortenURL = "fake_shorten_url"
+	const url = "fake_url"
+	const expiredAt = "fake_expired_date"
 
 	BeforeEach(func() {
 		ctx = context.Background()
@@ -68,11 +70,15 @@ var _ = Describe("Service", func() {
 			resp = httptest.NewRecorder()
 		})
 
+		AfterEach(func() {
+			Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+		})
+
 		JustBeforeEach(func() {
 			router.ServeHTTP(resp, req)
 		})
 
-		When("success", func() {
+		Context("success", func() {
 			BeforeEach(func() {
 				shortener = &dao.Shortener{
 					URL: "fake_url",
@@ -86,73 +92,63 @@ var _ = Describe("Service", func() {
 				}).Return(shortener, nil)
 			})
 
-			It("redircts with no error", func() {
-				Expect(resp.Code).To(Equal(message.URLRedirect))
-				Expect(resp.Result().Header.Get("Location")).To(Equal("/fake_url"))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+			When("shorten url found with no error", func() {
+				It("redirects url successfully", func() {
+					Expect(resp.Code).To(Equal(message.URLRedirect))
+					Expect(resp.Result().Header.Get("Location")).To(Equal("/fake_url"))
+				})
 			})
 		})
 
-		When("date expired", func() {
+		Context("fail", func() {
 			BeforeEach(func() {
 				req, err = http.NewRequestWithContext(ctx, "GET", "/test_get?shorten_url="+shortenURL, http.NoBody)
 				Expect(err).NotTo(HaveOccurred())
-
-				shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
-					ShortenURL: shortenURL,
-				}).Return(nil, dao.ErrExpiredat)
 			})
 
-			It("returns the url expired error", func() {
+			JustBeforeEach(func() {
 				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
-
 				Expect(err).NotTo(HaveOccurred())
-				Expect(respValue["error"]).To(Equal("this shorten url has already expired"))
 				Expect(respValue["request"]).To(Equal("redirect url request"))
-				Expect(resp.Code).To(Equal(message.URLExpired))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
-			})
-		})
-
-		When("shorten url not found", func() {
-			BeforeEach(func() {
-				req, err = http.NewRequestWithContext(ctx, "GET", "/test_get?shorten_url="+shortenURL, http.NoBody)
-				Expect(err).NotTo(HaveOccurred())
-
-				shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
-					ShortenURL: shortenURL,
-				}).Return(nil, dao.ErrShortenURLNotFound)
 			})
 
-			It("returns the url not found error", func() {
-				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
+			When("date expired", func() {
+				BeforeEach(func() {
+					shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
+						ShortenURL: shortenURL,
+					}).Return(nil, dao.ErrExpiredat)
+				})
 
-				Expect(err).NotTo(HaveOccurred())
-				Expect(respValue["error"]).To(Equal("this shorten url not found"))
-				Expect(respValue["request"]).To(Equal("redirect url request"))
-				Expect(resp.Code).To(Equal(message.BadRequest))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
-			})
-		})
-
-		When("internal server error", func() {
-			BeforeEach(func() {
-				req, err = http.NewRequestWithContext(ctx, "GET", "/test_get?shorten_url="+shortenURL, http.NoBody)
-				Expect(err).NotTo(HaveOccurred())
-
-				shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
-					ShortenURL: shortenURL,
-				}).Return(nil, errors.New("internal server error"))
+				It("returns the url expired error", func() {
+					Expect(respValue["error"]).To(Equal("this shorten url has already expired"))
+					Expect(resp.Code).To(Equal(message.URLExpired))
+				})
 			})
 
-			It("returns the url not found error", func() {
-				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
+			When("shorten url not found", func() {
+				BeforeEach(func() {
+					shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
+						ShortenURL: shortenURL,
+					}).Return(nil, dao.ErrShortenURLNotFound)
+				})
 
-				Expect(err).NotTo(HaveOccurred())
-				Expect(respValue["error"]).To(Equal("internal server error"))
-				Expect(respValue["request"]).To(Equal("redirect url request"))
-				Expect(resp.Code).To(Equal(message.ServiceError))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+				It("returns the shorten url not found error", func() {
+					Expect(respValue["error"]).To(Equal("this shorten url not found"))
+					Expect(resp.Code).To(Equal(message.BadRequest))
+				})
+			})
+
+			When("internal server error", func() {
+				BeforeEach(func() {
+					shortenerDAO.EXPECT().Get(req.Context(), &dao.Shortener{
+						ShortenURL: shortenURL,
+					}).Return(nil, errors.New("internal server error"))
+				})
+
+				It("returns the internal server error", func() {
+					Expect(respValue["error"]).To(Equal("internal server error"))
+					Expect(resp.Code).To(Equal(message.ServiceError))
+				})
 			})
 		})
 	})
@@ -162,8 +158,6 @@ var _ = Describe("Service", func() {
 			req       *http.Request
 			resp      *httptest.ResponseRecorder
 			id        uuid.UUID
-			url       string
-			expiredAt string
 			jsonData  []byte
 			respValue map[string]string
 			err       error
@@ -173,15 +167,17 @@ var _ = Describe("Service", func() {
 			resp = httptest.NewRecorder()
 		})
 
+		AfterEach(func() {
+			Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+		})
+
 		JustBeforeEach(func() {
 			router.ServeHTTP(resp, req)
 		})
 
-		When("success", func() {
+		Context("success", func() {
 			BeforeEach(func() {
 				id = uuid.New()
-				url = "fake_url"
-				expiredAt = "fake_expired_date"
 				jsonData = []byte(`{
 					"url": "fake_url",
 					"expiredAt": "fake_expired_date"
@@ -198,99 +194,91 @@ var _ = Describe("Service", func() {
 				}).Return(id, shortenURL, nil)
 			})
 
-			It("shorten url with no error", func() {
+			JustBeforeEach(func() {
 				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
-
 				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.Code).To(Equal(message.SuccessRequest))
-				Expect(respValue["id"]).To(Equal(id.String()))
-				Expect(respValue["shortenURL"]).To(Equal(shortenURL))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+			})
+
+			When("shorten url", func() {
+				It("returns shorten url and id with no error", func() {
+					Expect(resp.Code).To(Equal(message.SuccessRequest))
+					Expect(respValue["id"]).To(Equal(id.String()))
+					Expect(respValue["shortenURL"]).To(Equal(shortenURL))
+				})
 			})
 		})
 
-		When("argument error", func() {
-			BeforeEach(func() {
-				jsonData = []byte(`{
-					"url": "fake_url",
-				}`)
-
-				req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
-				Expect(err).NotTo(HaveOccurred())
-
-				req.Header.Set("Content-Type", "application/json")
-			})
-
-			It("returns the argument error", func() {
+		Context("fail", func() {
+			JustBeforeEach(func() {
 				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
-
 				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.Code).To(Equal(message.BadRequest))
-				Expect(respValue["error"]).To(Equal("argument error"))
 				Expect(respValue["request"]).To(Equal("shorten url request"))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
-			})
-		})
-
-		When("expriedAt argument has already expired", func() {
-			BeforeEach(func() {
-				url = "fake_url"
-				expiredAt = "fake_expired_date"
-				jsonData = []byte(`{
-					"url": "fake_url",
-					"expiredAt": "fake_expired_date"
-				}`)
-
-				req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
-				Expect(err).NotTo(HaveOccurred())
-
-				req.Header.Set("Content-Type", "application/json")
-
-				shortenerDAO.EXPECT().Shorten(req.Context(), &dao.Shortener{
-					URL:       url,
-					ExpiredAt: expiredAt,
-				}).Return(uuid.Nil, "", dao.ErrShortenURLFail)
 			})
 
-			It("returns the error that the argument expiredAt has already expired error", func() {
-				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
+			When("argument error", func() {
+				BeforeEach(func() {
+					jsonData = []byte(`{
+						"url": "fake_url",
+					}`)
 
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.Code).To(Equal(message.BadRequest))
-				Expect(respValue["error"]).To(Equal("expiredAt has already expired"))
-				Expect(respValue["request"]).To(Equal("shorten url request"))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
-			})
-		})
+					req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
+					Expect(err).NotTo(HaveOccurred())
 
-		When("internal server error", func() {
-			BeforeEach(func() {
-				url = "fake_url"
-				expiredAt = "fake_expired_date"
-				jsonData = []byte(`{
-					"url": "fake_url",
-					"expiredAt": "fake_expired_date"
-				}`)
+					req.Header.Set("Content-Type", "application/json")
+				})
 
-				req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
-				Expect(err).NotTo(HaveOccurred())
-
-				req.Header.Set("Content-Type", "application/json")
-
-				shortenerDAO.EXPECT().Shorten(req.Context(), &dao.Shortener{
-					URL:       url,
-					ExpiredAt: expiredAt,
-				}).Return(uuid.Nil, "", errors.New("internal server error"))
+				It("returns the argument error", func() {
+					Expect(resp.Code).To(Equal(message.BadRequest))
+					Expect(respValue["error"]).To(Equal("argument error"))
+				})
 			})
 
-			It("returns the internal server error", func() {
-				err = json.Unmarshal(resp.Body.Bytes(), &respValue)
+			When("expiredAt argument has already expired", func() {
+				BeforeEach(func() {
+					jsonData = []byte(`{
+						"url": "fake_url",
+						"expiredAt": "fake_expired_date"
+					}`)
 
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.Code).To(Equal(message.ServiceError))
-				Expect(respValue["error"]).To(Equal("internal server error"))
-				Expect(respValue["request"]).To(Equal("shorten url request"))
-				Expect(resp.Result().Body.Close()).NotTo(HaveOccurred())
+					req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
+					Expect(err).NotTo(HaveOccurred())
+
+					req.Header.Set("Content-Type", "application/json")
+
+					shortenerDAO.EXPECT().Shorten(req.Context(), &dao.Shortener{
+						URL:       url,
+						ExpiredAt: expiredAt,
+					}).Return(uuid.Nil, "", dao.ErrShortenURLFail)
+				})
+
+				It("returns the expired error", func() {
+					Expect(resp.Code).To(Equal(message.BadRequest))
+					Expect(respValue["error"]).To(Equal("expiredAt has already expired"))
+				})
+			})
+
+			When("internal server error", func() {
+				BeforeEach(func() {
+					jsonData = []byte(`{
+						"url": "fake_url",
+						"expiredAt": "fake_expired_date"
+					}`)
+
+					req, err = http.NewRequestWithContext(ctx, "POST", "/test_shorten", bytes.NewBuffer(jsonData))
+					Expect(err).NotTo(HaveOccurred())
+
+					req.Header.Set("Content-Type", "application/json")
+
+					shortenerDAO.EXPECT().Shorten(req.Context(), &dao.Shortener{
+						URL:       url,
+						ExpiredAt: expiredAt,
+					}).Return(uuid.Nil, "", errors.New("internal server error"))
+				})
+
+				It("returns the internal server error", func() {
+					Expect(resp.Code).To(Equal(message.ServiceError))
+					Expect(respValue["error"]).To(Equal("internal server error"))
+				})
 			})
 		})
 	})
